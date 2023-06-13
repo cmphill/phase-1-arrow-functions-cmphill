7639194dc7017ccd90304024e235cd986f95c4be
Cooper <cmphill@pm.me>
2023-06-13 11:37:42 -0600

    added the function expression and the arrow functions

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = function (first = 2000, second = 100) {
+    return (first/second)
+};
+const square = parameter => parameter * parameter;
+
+const add = (parameter1,parameter2) => parameter1 + parameter2
